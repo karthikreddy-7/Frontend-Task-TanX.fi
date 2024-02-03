44b6668a1c427e54fde5ce7f1907d93b17759dd2
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2024-02-03 10:58:05 +0530

    fixed json server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-function App() {
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const apiUrl =
+      process.env.REACT_APP_API_ENDPOINT || "http://localhost:8000";
+
+    // Fetch data when the component mounts
+    fetch(`${apiUrl}/products`)
+      .then((response) => response.json())
+      .then((data) => setProducts(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []); // The empty dependency array ensures that this effect runs only once on mount
+
   return (
-    <div>
-      <h1>Let's Go</h1>
+    <div className="text-black">
+      <h1>Product List</h1>
+      <ul>
+        {console.log(products)}
+        {products.map((product) => (
+          <li key={product.id}>{product.title}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
